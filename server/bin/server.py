9830e00b376f0908de4c109025fdc033b181660d
neozheng <andrewzheng@sina.cn>
2018-08-02 23:20:54 +0800

    FTP入口注释

--- server/bin/server.py
+++ server/bin/server.py
@@ -3,6 +3,8 @@ import os, sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
+# 入口
+
 if __name__ == "__main__":
     from core import management
 
